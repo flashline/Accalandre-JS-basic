dd14ac2a7c0e090b1354d3a61648e2a33508b986
jean-michel delettre <info@pixaline.net>
2019-09-10 14:00:38 +0200

    minor 10/09/19

--- ex01/index.js
+++ ex01/index.js
@@ -2,14 +2,20 @@
 
 
 // Les tableaux (type js Array)
-
  
 
 // les fonctions
 //
 // 		appel de fonctions fournies par js
-//
+
+
+// 		Créer ses propres fonctions
+
 
 // Les boucles et exécution conditionnelle 
 
+
 // Les objets (type js Object)
+
+
+// Les événements
